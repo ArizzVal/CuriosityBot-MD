7846dfeacf038a6505c2ae1a039e6e176638407b
Edwardofc <150191327+Edwardofc@users.noreply.github.com>
2024-02-23 23:53:48 -0500

    Actualizar ai-hd.js

--- plugins/ai-hd.js
+++ plugins/ai-hd.js
@@ -23,7 +23,7 @@ let img = await q.download?.()
 let error
 try {
 const This = await processing(img, "enlace")
-conn.sendFile(m.chat, This, '', '🧃 *Toma tu foto*\n\n' + cred.toString('utf-8'), m)
+conn.sendFile(m.chat, This, '', '📷 *Toma tu foto*', m)
 } catch (er) {
 error = true
 } finally {
